a795388819c9614f4b635eb957b914bda1f7aea6
miquel.ramirez@gmail.com <miquel.ramirez@gmail.com>
2018-11-22 10:19:59 +1100

    README and environments registry

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # pocrl-benchmarks
 Benchmarks for Planning, Optimal Control and Reinforcement Learning
+
+
+## Installation
+
+
+Invoke ```pip``` at the root of the folder where you cloned this repo
+
+```
+$ pip install --user - .
+```
--- src/yowie_gym/__init__.py
+++ src/yowie_gym/__init__.py
@@ -5,33 +5,5 @@ from gym.envs.registration import register
 import envs.tarski.random_lq
 import envs.tarski.car_park
 import envs.tarski.uav_platoon
+import envs.tarski.auv_navigation
 import envs.bullet
-
-register(
-    id='Bertsekas671-000-v1',
-    entry_point='envs.scalar:Bertsekas671_Env',
-    kwargs = {
-        'w_mu': 0.0,
-        'w_sigma': 1.0,
-        'x0': 75.0,
-        'horizon': 100
-    })
-
-register(
-    id='Bertsekas671-000-v2',
-    entry_point='envs.tarski.bertsekas_671:Environment',
-    kwargs = {
-        'mu_w': 0.0,
-        'sigma_w': 1.0,
-        'x0': 75.0,
-        'horizon': 100,
-        'dt': 0.1
-    }
-)
-
-register(
-    id='RechtLQR-v0',
-    entry_point = 'envs.recht_lqr:LQR_Env',
-    max_episode_steps=1000,
-    kwargs = {}
-)
